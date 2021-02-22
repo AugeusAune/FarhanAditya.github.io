8530b1309c157bcd4f9415e20f758d5c09577406
AugeusAune <farhanaditya134@gmail.com>
2021-02-22 17:15:54 +0700

    A

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const click = document.querySelector('.menu-toggler');
+const link = document.querySelector('.link');
+
+click.addEventListener('click', function() {
+    click.classList.toggle('active');
+    link.classList.toggle('active');
+    
+})
